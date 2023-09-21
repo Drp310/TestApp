6613917dc53861bc13b64a6e187b8f2f5a1e31e1
teinawa <helepoctb123@gmail.com>
2023-09-21 16:39:26 +0300

    pashla naxuy dura)))

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -5,9 +5,8 @@ import script from "./gulp-tasks/script";
 import imgMinAll from "./gulp-tasks/img";
 import fonts from "./gulp-tasks/fonts";
 import server from "./gulp-tasks/server";
-import libs from "./gulp-tasks/libs";
 
-const build = series(pugToHtml, libs, styles, script, imgMinAll, fonts);
+const build = series(pugToHtml, styles, script, imgMinAll, fonts);
 
 const start = series(build, server);
 
